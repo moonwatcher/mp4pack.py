ac86cf628862b9e1938eecfc88a6faaeb361a504
Lior Galanti <lior.galanti@gmail.com>
2013-06-25 05:46:27 +0300

    adjusting presets

--- queue.py
+++ queue.py
@@ -150,7 +150,7 @@ class Job(object):
     
     @property
     def document(self):
-        return json.dumps(self.execution, sort_keys=True, indent=4,  default=self.env.default_json_handler)
+        return json.dumps(self.execution, sort_keys=True, indent=4,  default=self.env.default_json_handler).encode('utf-8')
     
     
     def load(self):
