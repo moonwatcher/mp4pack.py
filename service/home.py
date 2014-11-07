df55f10f552f26b5a0f0e62793144cda4c6f5817
Lior Galanti <lior.galanti@gmail.com>
2014-11-06 20:04:11 -0500

    adjustments to home interface

--- service/home.py
+++ service/home.py
@@ -8,21 +8,25 @@ class HomeHandler(ResourceHandler):
         ResourceHandler.__init__(self, resolver, node)
         
     def fetch(self, query):
+        # ensure default language
+        query['parameter']['language']
+        
         if 'depend' in query['match']:
+            # Resolve the dependency document
+            dependee = self.resolver.resolve(query['match']['depend'].format(**query['parameter']))
             
-            # ensure default language
-            query['parameter']['language']
-            
-            if 'index' in query['branch']:
-                # Resolve the dependency document
-                dependee = self.resolver.resolve(query['match']['depend'].format(**query['parameter']))
-                if dependee is not None:
+            # If the document was resolved
+            if dependee is not None:
+                # and the branch has indexed parameters
+                # copy indexed parameters from the dependee's genealogy to the query parameter ontology
+                if 'index' in query['branch']:
                     for index in query['branch']['index']:
                         if index in dependee[u'head'][u'genealogy']:
                             query['parameter'][index] = dependee[u'head'][u'genealogy'][index]
                             
-                    query['sources'].append(dependee)
-                    
+                # append the resolved dependee to the sources list
+                query['sources'].append(dependee)
+                
     def parse(self, query):
         # Only create a new home document if the dependency is satisfied
         if query['sources']:
